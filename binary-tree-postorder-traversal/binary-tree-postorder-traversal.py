879ca80c163ad376bbb967cd331c942af3f84729
Neha Vari <vari.neha@gmail.com>
2022-07-14 18:09:15 +0530

    Time: 59 ms, Memory: 13.8 MB - LeetHub

--- binary-tree-postorder-traversal/binary-tree-postorder-traversal.py
+++ binary-tree-postorder-traversal/binary-tree-postorder-traversal.py
@@ -1,28 +1,30 @@
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
+'''
+Morris post order traversal
+'''
 class Solution:          
-    def postorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
-        ans, stack = [], []
-        while root or stack:
-            while root:
-                if root.right:
-                    stack.append(root.right)
-                stack.append(root)
-                root = root.left
-            root = stack.pop()
-            
-            if root.right and stack and stack[-1] == root.right:
-                stack.pop() 
-                stack.append(root) 
-                root = root.right 
+    def postorderTraversal(self, root: TreeNode) -> List[int]:
+        res = deque()
+        while root:
+            if root.right:
+                print("root.right", root.right.val)
+                temp = root.right
+                while temp.left and temp.left != root:
+                    temp = temp.left
+                if not temp.left:
+                    print("********", root.val, "********")
+                    res.appendleft(root.val)
+                    temp.left = root
+                    root = root.right
+                else:
+                    temp.left = None
+                    root = root.left
             else:
-                ans.append(root.val)
-                root = None
-
-        return ans
-    
-    
+                print("*****in else********", root.val)
+                res.appendleft(root.val)
+                if root.left:
+                    print("root.left", root.left.val)
+                else:
+                    print("root.left is None")
+                root = root.left
+                
+        return res
