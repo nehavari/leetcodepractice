5187a2923a642473dfaa878a4867ed4bc8fbfb72
Neha Vari <vari.neha@gmail.com>
2022-07-30 14:16:00 +0530

    Time: 110 ms, Memory: 14.7 MB - LeetHub

--- best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.py
+++ best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.py
@@ -6,20 +6,18 @@ class Solution:
         COOL_DOWN = 2
         size = len(prices)
         
-        @cache
-        def dp(i, status):
-            if i == size:
-                return 0
-            
-            do_nothing = dp(i + 1, status)
-                    
-            if status == HOLDING:
-                do_something = prices[i] + dp(i + 1, COOL_DOWN)
-            elif status == COOL_DOWN:
-                do_something = dp(i + 1, NO_HOLDING)
-            else:
-                do_something = -prices[i] + dp(i + 1, HOLDING)
-            
-            return max(do_nothing, do_something)
+        dp = [[0] * 3 for _ in range(size + 1)]
         
-        return dp(0, NO_HOLDING)
+        for i in range(size - 1, -1, -1):
+            for status in (HOLDING, NO_HOLDING, COOL_DOWN):
+                do_nothing = dp[i + 1][status]
+                if status == HOLDING:
+                    do_something = prices[i] + dp[i+1][COOL_DOWN]
+                elif status == COOL_DOWN:
+                    do_something = dp[i+1][NO_HOLDING]
+                else:
+                    do_something = -prices[i] + dp[i + 1][HOLDING]
+                    
+                dp[i][status] = max(do_nothing, do_something)
+                
+        return dp[0][NO_HOLDING]
