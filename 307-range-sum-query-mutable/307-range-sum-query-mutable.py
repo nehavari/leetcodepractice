4fc7fabb11f50140bd4b600426d7373c9d982888
Neha Vari <vari.neha@gmail.com>
2022-08-01 15:11:19 +0530

    Time: 1668 ms (91.65%), Space: 31.1 MB (71.61%) - LeetHub

--- 307-range-sum-query-mutable/307-range-sum-query-mutable.py
+++ 307-range-sum-query-mutable/307-range-sum-query-mutable.py
@@ -3,33 +3,42 @@ class NumArray:
 
     def __init__(self, nums: List[int]):
         self.nums = nums
-        self.buffer_len = math.ceil(math.sqrt(len(nums)))
-        self.buffer = [0] * self.buffer_len
-        for index in range(len(self.nums)):
-            self.buffer[index // self.buffer_len] += self.nums[index]
+        self.size = len(self.nums)
+        self.segTree = [0] * (2 * self.size)
         
-        
+        # assign leaf nodes
+        for i, j in zip(range(self.size, 2 * self.size), range(0, self.size)):
+            self.segTree[i] = self.nums[j]
+            
+        for i in range(self.size - 1, 0, -1):
+            self.segTree[i] = self.segTree[2 * i] + self.segTree[2 * i + 1]
+            
     def update(self, index: int, val: int) -> None:
-        buffer_ind = index // self.buffer_len
-        self.buffer[buffer_ind] = self.buffer[buffer_ind] - self.nums[index] + val
-        self.nums[index] = val
-      
+        index = index + self.size
+        self.segTree[index] = val
+        parent = index // 2
+        while parent != 0:
+            left = 2 * parent
+            right = 2 * parent + 1
+            self.segTree[parent] = self.segTree[left] + self.segTree[right]
+            parent = parent // 2
+        
     def sumRange(self, left: int, right: int) -> int:
-        sumR = 0
-        startBlock = left // self.buffer_len
-        endBlock = right // self.buffer_len
-        if startBlock == endBlock:
-            for i in range(left, right + 1):
-                sumR += self.nums[i]
-        else:
-            for i in range(left, (startBlock + 1) * self.buffer_len):
-                sumR += self.nums[i]
-            for i in range(startBlock + 1, endBlock):
-                sumR += self.buffer[i]
-            for i in range(endBlock * self.buffer_len, right + 1):
-                sumR += self.nums[i]
-                
-        return sumR
+        left = left + self.size
+        right = right + self.size
+        ssum = 0 
+        while left <= right:
+            if left % 2 == 1:
+                ssum += self.segTree[left]
+                left += 1
+            if right % 2 == 0:
+                ssum += self.segTree[right]
+                right -= 1
+          
+            left //= 2
+            right //= 2
+        
+        return ssum
 
 
 # Your NumArray object will be instantiated and called as such:
